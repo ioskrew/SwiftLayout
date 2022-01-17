4a1f80fbab054343fecd2c99862507a5319efd9f
oozoofrog <oozoofrog@gmail.com>
2022-01-17 22:44:11 +0900

    update to origin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AutolayoutDSL
 Autolayout DSL library
+
+목표는 인터페이스 빌더와 같은 코드입니다.
--- Sources/AutolayoutDSL/Extensions/UIView+Autolayout.swift
+++ Sources/AutolayoutDSL/Extensions/UIView+Autolayout.swift
@@ -7,9 +7,4 @@ extension AutolayoutComponent where Self: UIView {
 }
 
 extension UIView: AutolayoutComponent {
-    
-    public func fill<C: AutolayoutComponent>(@AutolayoutBuilder content: () -> C) -> some AutolayoutComponent {
-        Layout.FillUp(to: self, content: content)
-    }
-    
 }
