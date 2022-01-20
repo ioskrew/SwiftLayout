2ea435de4d21a910ce1652a12fc71bacdd934cdd
oozoofrog <oozoofrog@gmail.com>
2022-01-20 23:16:25 +0900

    name change to SwiftLayout

--- Tests/SwiftLayoutTests/SwiftLayoutTests.swift
+++ Tests/SwiftLayoutTests/SwiftLayoutTests.swift
@@ -1,11 +1,10 @@
 import XCTest
-@testable import AutolayoutDSL
+@testable import SwiftLayout
 
-final class AutolayoutDSLTests: XCTestCase {
+final class SwiftLayoutTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AutolayoutDSL().text, "Hello, World!")
     }
 }
