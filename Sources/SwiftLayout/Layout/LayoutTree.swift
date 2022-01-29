a5e109bee3160fdbd245c8b91a15f87d72db156b
oozoofrog <oozoofrog@gmail.com>
2022-01-30 02:01:26 +0900

    이렇게 LayoutTree를 또 재발명합니다 어허허

---
+++ Sources/SwiftLayout/Layout/LayoutTree.swift
@@ -0,0 +1,16 @@
+//
+//  LayoutTree.swift
+//  
+//
+//  Created by oozoofrog on 2022/01/30.
+//
+
+import Foundation
+import UIKit
+
+struct LayoutTree: Layoutable {
+    
+    let view: UIView
+    let subviews: [UIView]
+    
+}
