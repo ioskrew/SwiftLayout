ff6a704a1074d1b6dab8f411eb4d98601caf6b71
oozoofrog <oozoofrog@gmail.com>
2022-01-30 18:36:11 +0900

    LayoutTree debug description 추가

--- Sources/SwiftLayout/Layout/LayoutTree.swift
+++ Sources/SwiftLayout/Layout/LayoutTree.swift
@@ -8,11 +8,19 @@
 import Foundation
 import UIKit
 
-public class LayoutTree: Layoutable {
+public class LayoutTree: Layoutable, CustomDebugStringConvertible {
     
     internal init(view: UIView, subtree: [LayoutTree] = []) {
         self.view = view
         self.subtrees = subtree
+    }
+    
+    deinit {
+        #if !DEBUG
+        print("<====DEINIT LAYOUT_TREE=====>")
+        print(self.debugDescription)
+        print("<\\===DEINIT LAYOUT_TREE=====>")
+        #endif
     }
     
     let view: UIView
@@ -35,4 +43,17 @@ public class LayoutTree: Layoutable {
         self
     }
     
+    public var debugDescription: String {
+        var tag = TagDescriptor(view).debugDescription
+        if !subtrees.isEmpty {
+            let subs = subtrees.map(\.debugDescription)
+            let count = tag.count
+            let elementPrefix = " ".repeated(count + 3)
+            let parenthesisPrefix = " ".repeated(count)
+            tag += " {\n"
+            tag += subs.map(elementPrefix+).joined(separator: "\n")
+            tag += "\n" + parenthesisPrefix + " }\n"
+        }
+        return tag
+    }
 }
