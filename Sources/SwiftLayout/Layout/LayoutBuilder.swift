8436669ff8fc11d89d6fe43537feee80d5c2e1c9
oozoofrog <oozoofrog@gmail.com>
2022-02-01 00:07:40 +0900

    test failed: PairLayout type check
    - 기존 Layout 구현체 제거

--- Sources/SwiftLayout/Layout/LayoutBuilder.swift
+++ Sources/SwiftLayout/Layout/LayoutBuilder.swift
@@ -13,21 +13,4 @@ public struct LayoutBuilder {
     public static func buildBlock<Component>(_ component: Component) -> Component where Component: Layout {
         component
     }
-    
-    public static func buildArray(_ components: [Layout]) -> Layout {
-        LayoutableComponents(components)
-    }
-    
-    public static func buildOptional(_ component: Layout?) -> Layout {
-        component ?? EmptyLayout()
-    }
-    
-    public static func buildEither(first component: Layout) -> Layout {
-        component
-    }
-    
-    public static func buildEither(second component: Layout) -> Layout {
-        component
-    }
-    
 }
