0378dd9699830b2952c831cb980ee185acb92154
oozoofrog <oozoofrog@gmail.com>
2022-02-01 00:23:17 +0900

    test succeed: SuperSubLayout add subview

--- Sources/SwiftLayout/Layout/Layouts/SuperSubLayout.swift
+++ Sources/SwiftLayout/Layout/Layouts/SuperSubLayout.swift
@@ -8,15 +8,35 @@
 import Foundation
 import UIKit
 
-public struct SuperSubLayout<SuperView, Sub>: Layout where SuperView: UIView, Sub: Layout {
+public struct SuperSubLayout<SuperView, Sub>: Layout, ViewLayout where SuperView: UIView, Sub: Layout {
     
-    let superview: SuperView
+    internal init(superview: SuperView, subLayout: Sub) {
+        self.view = superview
+        self.subLayout = subLayout
+    }
+    
+    let view: SuperView
     let subLayout: Sub
     
+    func addSubview(_ view: UIView) {
+        self.view.addSubview(view)
+    }
+    
+    func attachSuperlayout(_ superlayout: ViewLayout) {
+        superlayout.addSubview(view)
+    }
+    
     public var equation: AnyHashable {
-        let superHash = superview.equation
+        let superHash = view.equation
         let subHash = subLayout.equation
         return AnyHashable([superHash, subHash])
     }
     
+}
+
+extension SuperSubLayout where Sub: ViewLayout {
+    func active() -> AnyLayout {
+        self.subLayout.attachSuperlayout(self)
+        return AnyLayout(self)
+    }
 }
