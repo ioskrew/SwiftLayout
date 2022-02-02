c94f2e28148c9856083170e0314cc4742dd0f1ce
oozoofrog <oozoofrog@gmail.com>
2022-02-02 14:33:12 +0900

    test failed: dsl type check of constraint

--- Tests/SwiftLayoutTests/ConstraintsTests.swift
+++ Tests/SwiftLayoutTests/ConstraintsTests.swift
@@ -6,30 +6,147 @@
 //
 
 import XCTest
+import SwiftLayout
 
 class ConstraintsTests: XCTestCase {
 
+    var deactivatable: AnyDeactivatable?
+    
+    var root = UIView().viewTag.root
+    var child = UIView().viewTag.child
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        root = UIView().viewTag.root
+        child = UIView().viewTag.child
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        deactivatable = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testTypeOfAnchors() {
+        
     }
+//    func testAxisAnchors() throws {
+//        deactivatable = root {
+//            child
+//        }.active()
+//
+//        XCTAssertEqual(child.superview, root)
+//        XCTAssertFalse(child.translatesAutoresizingMaskIntoConstraints)
+//        XCTAssertEqual(child.constraints.count, 1)
+//        let constraint = child.constraints.first!
+//        XCTAssertConstraint(constraint,
+//                            first: .init(item: child, attribute: .top),
+//                            second: .init(item: root, attribute: .top))
+//    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+}
+
+typealias Attribute = NSLayoutConstraint.Attribute
+typealias Relation = NSLayoutConstraint.Relation
+struct ConstraintItem<Item> where Item: NSObjectProtocol {
+    let item: Item
+    let attribute: Attribute
+}
+func XCTAssertConstraint<FirstItem, SecondItem>(_ constraint: NSLayoutConstraint,
+                                                first: ConstraintItem<FirstItem>,
+                                                second: ConstraintItem<SecondItem>?,
+                                                relation: Relation = .equal, file: StaticString = #file, line: UInt = #line) where FirstItem: NSObjectProtocol, SecondItem: NSObjectProtocol {
+    guard constraint.relation == relation else {
+        XCTFail("relation: constraint.\(constraint.relation) != \(relation)", file: file, line: line)
+        return
+    }
+    if let firstItem = constraint.firstItem {
+        guard first.item.isEqual(firstItem) else {
+            XCTFail("first item: constraint.\(firstItem) != \(first.item)", file: file, line: line)
+            return
+        }
+        guard first.attribute == constraint.firstAttribute else {
+            XCTFail("first attribute: constraint.\(constraint.firstAttribute) != \(first.attribute)", file: file, line: line)
+            return
+        }
+    } else {
+        XCTFail("first item is nil", file: file, line: line)
+    }
+    if let second = second {
+        if let secondItem = constraint.secondItem {
+            guard second.item.isEqual(secondItem) else {
+                XCTFail("first item: constraint.\(secondItem) != \(second.item)", file: file, line: line)
+                return
+            }
+            guard second.attribute == constraint.secondAttribute else {
+                XCTFail("first attribute: constraint.\(constraint.secondAttribute) != \(second.attribute)", file: file, line: line)
+                return
+            }
+        } else {
+            XCTFail("second item is nil", file: file, line: line)
         }
     }
+}
 
+extension Attribute: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .left:
+            return "NSLayoutConstraint.Attribute.left"
+        case .right:
+            return "NSLayoutConstraint.Attribute.right"
+        case .top:
+            return "NSLayoutConstraint.Attribute.top"
+        case .bottom:
+            return "NSLayoutConstraint.Attribute.bottom"
+        case .leading:
+            return "NSLayoutConstraint.Attribute.leading"
+        case .trailing:
+            return "NSLayoutConstraint.Attribute.trailing"
+        case .width:
+            return "NSLayoutConstraint.Attribute.width"
+        case .height:
+            return "NSLayoutConstraint.Attribute.height"
+        case .centerX:
+            return "NSLayoutConstraint.Attribute.centerX"
+        case .centerY:
+            return "NSLayoutConstraint.Attribute.centerY"
+        case .lastBaseline:
+            return "NSLayoutConstraint.Attribute.lastBaseline"
+        case .firstBaseline:
+            return "NSLayoutConstraint.Attribute.firstBaseline"
+        case .leftMargin:
+            return "NSLayoutConstraint.Attribute.leftMargin"
+        case .rightMargin:
+            return "NSLayoutConstraint.Attribute.rightMargin"
+        case .topMargin:
+            return "NSLayoutConstraint.Attribute.topMargin"
+        case .bottomMargin:
+            return "NSLayoutConstraint.Attribute.bottomMargin"
+        case .leadingMargin:
+            return "NSLayoutConstraint.Attribute.leadingMargin"
+        case .trailingMargin:
+            return "NSLayoutConstraint.Attribute.trailingMargin"
+        case .centerXWithinMargins:
+            return "NSLayoutConstraint.Attribute.centerXWithinMargins"
+        case .centerYWithinMargins:
+            return "NSLayoutConstraint.Attribute.centerYWithinMargins"
+        case .notAnAttribute:
+            return "NSLayoutConstraint.Attribute.notAnAttribute"
+        @unknown default:
+            return "NSLayoutConstraint.Attribute.unknown"
+        }
+    }
+}
+
+extension Relation: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .lessThanOrEqual:
+            return "NSLayoutConstraint.Attribute.lessThanOrEqual"
+        case .equal:
+            return "NSLayoutConstraint.Attribute.equal"
+        case .greaterThanOrEqual:
+            return "NSLayoutConstraint.Attribute.greaterThanOrEqual"
+        @unknown default:
+            return "NSLayoutConstraint.Attribute.unknown"
+        }
+    }
 }
