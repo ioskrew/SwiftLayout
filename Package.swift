5a6bc22266833d263f618ce5922aa141ede8caa8
oozoofrog <oozoofrog@gmail.com>
2022-02-05 23:33:55 +0900

    테스트성공: new layout tree active

--- Package.swift
+++ Package.swift
@@ -25,5 +25,6 @@ let package = Package(
         .testTarget(
             name: "SwiftLayoutTests",
             dependencies: ["SwiftLayout"]),
+        .testTarget(name: "IndependentTests"),
     ]
 )
---
+++ Tests/IndependentTests/LayoutTests.swift
@@ -0,0 +1,141 @@
+//
+//  LayoutTests.swift
+//  
+//
+//  Created by oozoofrog on 2022/02/05.
+//
+
+import Foundation
+import XCTest
+
+extension UIView: _Layout {}
+
+protocol _Layout {
+    func active()
+    func attachSuperview(_ superview: UIView?)
+    func attachSuperview()
+}
+
+extension _Layout {
+    func active() {
+        attachSuperview()
+    }
+    func attachSuperview() {
+        attachSuperview(nil)
+    }
+}
+
+protocol _LayoutContainable: _Layout {
+    var layouts: [_Layout] { get }
+}
+
+extension _LayoutContainable {
+    func attachSuperview(_ superview: UIView?) {
+        for layout in layouts {
+            layout.attachSuperview(superview)
+        }
+    }
+}
+
+protocol _LayoutViewContainable: _LayoutContainable {
+    var view: UIView { get }
+}
+
+extension _LayoutViewContainable {
+    func attachSuperview(_ superview: UIView?) {
+        superview?.addSubview(self.view)
+        for layout in layouts {
+            layout.attachSuperview(self.view)
+        }
+    }
+}
+
+extension _Layout where Self: _LayoutContainable, Self: Collection, Element == _Layout {
+    var layouts: [_Layout] {
+        map({ $0 as _Layout })
+    }
+}
+
+extension Array: _Layout where Self.Element == _Layout {}
+extension Array: _LayoutContainable where Self.Element == _Layout {}
+
+struct _OptionalLayout<L>: _Layout where L: _Layout {
+    let layout: L?
+    
+    func attachSuperview(_ superview: UIView?) {
+        layout?.attachSuperview(superview)
+    }
+}
+
+
+struct _ViewLayout<L>: _LayoutViewContainable where L: _LayoutContainable {
+    var view: UIView
+    var layoutable: L
+    
+    var layouts: [_Layout] {
+        layoutable.layouts
+    }
+}
+
+@resultBuilder
+struct _LayoutBuilder {
+    static func buildBlock(_ components: _Layout...) -> [_Layout] {
+        components
+    }
+    static func buildEither(first component: [_Layout]) -> [_Layout] {
+        component
+    }
+    static func buildEither(second component: [_Layout]) -> [_Layout] {
+        component
+    }
+    static func buildOptional(_ component: [_Layout]?) -> [_Layout] {
+        component ?? []
+    }
+}
+
+extension _Layout where Self: UIView {
+    
+    func callAsFunction<L>(@_LayoutBuilder _ build: () -> L) -> _ViewLayout<L> where L: _Layout {
+        _ViewLayout(view: self, layoutable: build())
+    }
+    
+    func attachSuperview(_ superview: UIView?) {
+        superview?.addSubview(self)
+    }
+}
+
+final class LayoutTests: XCTestCase {
+
+    var root = UIView()
+    var child = UIView()
+    var grandChild1 = UIView()
+    var grandChild2 = UIView()
+    
+    override func setUp() {
+        root = UIView()
+        child = UIView()
+        grandChild1 = UIView()
+        grandChild2 = UIView()
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testLayoutTree() {
+        let layout: some _Layout = root {
+            child {
+                grandChild1
+                grandChild2
+            }
+        }
+        
+        print(layout)
+        layout.active()
+        
+        XCTAssertEqual(grandChild1.superview, child)
+        XCTAssertEqual(grandChild2.superview, child)
+        XCTAssertEqual(child.superview, root)
+    }
+    
+}
