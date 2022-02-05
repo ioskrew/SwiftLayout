f14aa9380e54222d6c4ce02a8ee4c037a6d2d9e5
oozoofrog <oozoofrog@gmail.com>
2022-02-05 10:29:42 +0900

    ConstraintBinder를 ConstraintBuilder의 nested type으로 만들고 이름을 Binding으로 수정

--- Sources/SwiftLayout/Layout/Constraint/Extension/UIView+Constraint.swift
+++ Sources/SwiftLayout/Layout/Constraint/Extension/UIView+Constraint.swift
@@ -10,8 +10,8 @@ import UIKit
 
 extension Layout where Self: UIView {
     
-    public func constraint<C>(@ConstraintBuilder _ content: (ConstraintBinder) -> C) -> ConstraintLayout<Self, C> where C: Constraint {
-        .init(view: self, constraints: content(ConstraintBinder()))
+    public func constraint<C>(@ConstraintBuilder _ content: () -> C) -> ConstraintLayout<Self, C> where C: Constraint {
+        .init(view: self, constraints: content())
     }
     
 }
