2956abab43731522ca13f15dbd51f21c9e137874
oozoofrog <oozoofrog@gmail.com>
2022-02-08 18:49:45 +0900

    인터페이스 혹은 구현을 테스트 하기 위한 테스트 케이스 추가

---
+++ Tests/SwiftLayoutTests/DSLIntenttionTests.swift
@@ -0,0 +1,25 @@
+//
+//  DSLIntentionTests.swift
+//  
+//
+//  Created by oozoofrog on 2022/02/08.
+//
+
+import Foundation
+import XCTest
+import SwiftLayout
+
+///
+/// 이 테스트 케이스에서는 구현이 아닌 인터페이스 혹은
+/// 구현을 테스트 합니다.
+final class DSLIntentionsTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+    
+    override func tearDownWithError() throws {
+        
+    }
+    
+}
