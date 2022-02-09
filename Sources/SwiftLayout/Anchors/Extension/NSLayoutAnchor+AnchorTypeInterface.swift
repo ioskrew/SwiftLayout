04c407ad7c367ff0a24d475e6490a1bf1f8c83f9
oozoofrog <oozoofrog@gmail.com>
2022-02-09 10:50:54 +0900

    remove constraint inheritance from NSLayoutAnchor

---
+++ Sources/SwiftLayout/Anchors/Extension/NSLayoutAnchor+AnchorTypeInterface.swift
@@ -0,0 +1,11 @@
+//
+//  NSLayoutAnchor+Constraint.swift
+//  
+//
+//  Created by oozoofrog on 2022/02/09.
+//
+
+import Foundation
+import UIKit
+
+extension NSLayoutAnchor: AnchorTypeInterface {}
