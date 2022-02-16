e46e1495104d3172175ea3de88555367add76a4b
oozoofrog <oozoofrog@gmail.com>
2022-02-16 20:12:06 +0900

    add SwiftLayoutPrinter.ViewToken

--- Sources/SwiftLayout/Printing/SwiftLayoutPrinter.swift
+++ Sources/SwiftLayout/Printing/SwiftLayoutPrinter.swift
@@ -16,6 +16,31 @@ public struct SwiftLayoutPrinter {
     let view: UIView
     
     public func print() -> String {
-        ""
+        view.token.identifier
+    }
+    
+    final class ViewToken: CustomStringConvertible {
+        internal init(identifier: String, subtokens: [SwiftLayoutPrinter.ViewToken]) {
+            self.identifier = identifier
+            self.subtokens = subtokens
+        }
+        
+        let identifier: String
+        let subtokens: [ViewToken]
+        
+        var description: String {
+            var identifiers: [String] = [identifier]
+            identifiers.append(contentsOf: subtokens.map({ token in
+                "\t" + token.identifier
+            }))
+            return identifiers.joined(separator: "\n")
+        }
+    }
+
+}
+
+fileprivate extension UIView {
+    var token: SwiftLayoutPrinter.ViewToken {
+        SwiftLayoutPrinter.ViewToken(identifier: tagDescription, subtokens: subviews.map(\.token))
     }
 }
