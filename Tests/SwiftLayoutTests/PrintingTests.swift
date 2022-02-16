0471035c27b8e6e94e05bffa791ba5c3e30a34c4
oozoofrog <oozoofrog@gmail.com>
2022-02-16 20:21:51 +0900

    test succeed two depth of views

--- Tests/SwiftLayoutTests/PrintingTests.swift
+++ Tests/SwiftLayoutTests/PrintingTests.swift
@@ -10,6 +10,8 @@ import SwiftLayout
 
 class PrintingTests: XCTestCase {
     
+    var deactivable: Deactivable?
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -22,7 +24,7 @@ class PrintingTests: XCTestCase {
         let root = UIView().viewTag.root
         let child = UIView().viewTag.child
         
-        let deactivable = root {
+        deactivable = root {
             child
         }.active()
         
@@ -30,6 +32,50 @@ class PrintingTests: XCTestCase {
         let expect = """
         root:UIView {
         \tchild:UIView
+        }
+        """
+        let result = SwiftLayoutPrinter(view: root).print()
+        print(result)
+        XCTAssertEqual(result, expect)
+    }
+    
+    func testTwoViews() throws {
+        let root = UIView().viewTag.root
+        let a = UIView().viewTag.a
+        let b = UIView().viewTag.b
+        
+        deactivable = root {
+            a
+            b
+        }.active()
+        
+        let expect = """
+        root:UIView {
+        \ta:UIView
+        \tb:UIView
+        }
+        """
+        let result = SwiftLayoutPrinter(view: root).print()
+        print(result)
+        XCTAssertEqual(result, expect)
+    }
+    
+    func testTwoDepthOfViews() throws {
+        let root = UIView().viewTag.root
+        let child = UIView().viewTag.child
+        let grandchild = UIView().viewTag.grandchild
+        
+        deactivable = root {
+            child {
+                grandchild
+            }
+        }.active()
+        
+        let expect = """
+        root:UIView {
+        \tchild:UIView {
+        \t\tgrandchild:UIView
+        \t}
         }
         """
         let result = SwiftLayoutPrinter(view: root).print()
