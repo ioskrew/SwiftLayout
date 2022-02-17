5f711dc645e6de13da1ac575280f7ed81f843f70
oozoofrog <oozoofrog@gmail.com>
2022-02-17 14:05:24 +0900

    - modification name of AccessibilityIdentifierUpdater to IdentifierUpdater - IdentifierUpdater now can return identifiers and addresses

---
+++ Sources/SwiftLayout/Util/AddressDescript.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by oozoofrog on 2022/02/17.
+//
+
+import Foundation
+
+struct AddressDescriptor<Object>: CustomStringConvertible where Object: AnyObject {
+    let description: String
+    
+    init(_ object: Object) {
+        self.description = Unmanaged<Object>.passUnretained(object).toOpaque().debugDescription + ":\(type(of: object))"
+    }
+}
