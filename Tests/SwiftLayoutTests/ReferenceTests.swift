34bbe0ff49539c45b7d0cb8368786b08f36a9d9e
oozoofrog <oozoofrog@gmail.com>
2022-03-05 23:20:23 +0900

    Update Tests/SwiftLayoutTests/ReferenceTests.swift

--- Tests/SwiftLayoutTests/ReferenceTests.swift
+++ Tests/SwiftLayoutTests/ReferenceTests.swift
@@ -15,7 +15,6 @@ class ReferenceTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
     var view: SelfReferenceView?
