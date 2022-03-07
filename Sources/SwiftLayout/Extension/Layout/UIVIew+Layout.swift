8c4b2b4d1e89f2ada8ffbb14b059a936fd7e3faf
gmlwhdtjd <gmlwhdtjd@naver.com>
2022-03-07 16:50:14 +0900

    update DocC of UIVIew+Layout

--- Sources/SwiftLayout/Extension/Layout/UIVIew+Layout.swift
+++ Sources/SwiftLayout/Extension/Layout/UIVIew+Layout.swift
@@ -8,14 +8,64 @@
 import UIKit
 
 extension UIView {
+    ///
+    /// Create a ``ViewLayout`` containing this view and the sublayouts.
+    ///
+    /// Sublayouts contained within the builder block are added to the view hierarchy through **addSubview(_:)** to the view.
+    ///
+    /// - Parameter build: A ``LayoutBuilder`` that  create sublayouts of this view.
+    /// - Returns: An ``ViewLayout`` that wraps this view and contains sublayouts .
+    ///
     public func callAsFunction<L: Layout>(@LayoutBuilder _ build: () -> L) -> some Layout {
         ViewLayout(self, sublayout: build())
     }
     
+    //
+    /// Create an ``AnchorsLayout`` containing the  ``Anchors`` of this view.
+    ///
+    /// ``Anchors`` express **NSLayoutConstraint** and can be applied through this method.
+    /// ```swift
+    /// // The constraint of the view can be expressed as follows.
+    ///
+    /// subView.anchors {
+    ///     Anchors(.top).equalTo(rootView, constant: 10)
+    ///     Anchors(.centerX).equalTo(rootView)
+    ///     Anchors(.width, .height).equalTo(rootView).setMultiplier(0.5)
+    /// }
+    ///
+    /// // The following code performs the same role as the code above.
+    ///
+    /// NSLayoutConstraint.activate([
+    ///     subView.topAnchor.constraint(equalTo: rootView.topAnchor, constant: 10),
+    ///     subView.centerXAnchor.constraint(equalTo: rootView.centerXAnchor),
+    ///     subView.widthAnchor.constraint(equalTo: rootView.widthAnchor, multiplier: 0.5),
+    ///     subView.heightAnchor.constraint(equalTo: rootView.heightAnchor, multiplier: 0.5)
+    /// ])
+    /// ```
+    ///
+    /// - Parameter build: A ``AnchorsBuilder`` that  create ``Anchors`` to be applied to this layout
+    /// - Returns: An ``AnchorsLayout`` that wraps this view and contains the anchors .
+    ///
     public func anchors(@AnchorsBuilder _ build: () -> Anchors) -> some Layout {
         AnchorsLayout(layout: ViewLayout(self, sublayout: EmptyLayout()), anchors: build())
     }
     
+    ///
+    /// Create a ``SublayoutLayout`` containing the sublayouts of this view.
+    ///
+    /// Sublayouts contained within the builder block are added to the view hierarchy through **addSubview(_:)** to the view.
+    /// ```swift
+    /// // The hierarchy of views can be expressed as follows,
+    /// // and means that UILabel is a subview of UIView.
+    ///
+    /// UIView().sublayout {
+    ///     UILabel()
+    /// }
+    /// ```
+    ///
+    /// - Parameter build: A ``LayoutBuilder`` that  create sublayouts of this view.
+    /// - Returns: An ``SublayoutLayout`` that wraps this view and contains sublayouts .
+    ///
     public func sublayout<L: Layout>(@LayoutBuilder _ build: () -> L) -> some Layout {
         SublayoutLayout(ViewLayout(self, sublayout: EmptyLayout()), build())
     }
