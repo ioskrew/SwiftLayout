9aa8a0b2c2be37bd7bd2cf2eb847acf744710611
oozoofrog <oozoofrog@gmail.com>
2022-03-12 20:31:58 +0900

    add SLView* for UIKit and AppKit

--- Sources/SwiftLayout/Extension/Kit/ViewAlias.swift
+++ Sources/SwiftLayout/Extension/Kit/ViewAlias.swift
@@ -7,20 +7,40 @@
 
 import Foundation
 
-#if canImport(AppKit)
-import AppKit
-public typealias UIView = AppKit.NSView
-public typealias UILayoutGuide = AppKit.NSLayoutGuide
-public typealias UIViewController = AppKit.NSViewController
+#if canImport(UIKit)
+import UIKit
+public typealias SLView = UIView
+public typealias SLLayoutGuide = UILayoutGuide
+public typealias SLViewController = UIViewController
+public typealias SLLayoutConstraint = NSLayoutConstraint
+public typealias SLLayoutXAxisAnchor = NSLayoutXAxisAnchor
+public typealias SLLayoutYAxisAnchor = NSLayoutYAxisAnchor
+public typealias SLLayoutDimension = NSLayoutDimension
 #if canImport(SwiftUI)
 import SwiftUI
-public typealias UIViewRepresentable = SwiftUI.NSViewRepresentable
-public typealias UIViewRepresentableContext = SwiftUI.NSViewRepresentableContext
-public typealias UIViewControllerRepresentable = SwiftUI.NSViewControllerRepresentable
-public typealias UIViewControllerRepresentableContext = SwiftUI.NSViewControllerRepresentableContext
+public typealias SLViewRepresentable = UIViewRepresentable
+public typealias SLViewRepresentableContext = UIViewRepresentableContext
+public typealias SLViewControllerRepresentable = UIViewControllerRepresentable
+public typealias SLViewControllerRepresentableContext = UIViewControllerRepresentableContext
+#endif
+#elseif canImport(AppKit)
+import AppKit
+public typealias SLView = NSView
+public typealias SLLayoutGuide = NSLayoutGuide
+public typealias SLViewController = NSViewController
+public typealias SLLayoutConstraint = NSLayoutConstraint
+public typealias SLLayoutXAxisAnchor = NSLayoutXAxisAnchor
+public typealias SLLayoutYAxisAnchor = NSLayoutYAxisAnchor
+public typealias SLLayoutDimension = NSLayoutDimension
+#if canImport(SwiftUI)
+import SwiftUI
+public typealias SLViewRepresentable = NSViewRepresentable
+public typealias SLViewRepresentableContext = NSViewRepresentableContext
+public typealias SLViewControllerRepresentable = NSViewControllerRepresentable
+public typealias SLViewControllerRepresentableContext = NSViewControllerRepresentableContext
 #endif
 
-extension NSView {
+extension SLView {
     public func setNeedsLayout() {}
     public func layoutIfNeeded() {
         layoutSubtreeIfNeeded()
