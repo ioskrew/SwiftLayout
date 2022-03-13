bdd404179f4b0a2c8acec995fd8168a0b473fd7a
oozoofrog <oozoofrog@gmail.com>
2022-03-14 00:50:42 +0900

    add shortDescription to NSLayoutConstraint

--- Sources/SwiftLayout/Extension/NSLayoutConstraint+Description.swift
+++ Sources/SwiftLayout/Extension/NSLayoutConstraint+Description.swift
@@ -12,6 +12,65 @@ import UIKit
 import AppKit
 #endif
 
+extension NSLayoutConstraint {
+    
+    var firstShort: String? {
+        if let view = self.firstItem as? SLView {
+            return view.tagDescription
+        } else if let guide = self.firstItem as? SLLayoutGuide, let view = guide.owningView {
+            return view.tagDescription + ".safeAreaLayoutGuide"
+        } else {
+            return nil
+        }
+    }
+    
+    var secondShort: String? {
+        if let view = self.secondItem as? SLView {
+            return view.tagDescription
+        } else if let guide = self.secondItem as? SLLayoutGuide, let view = guide.owningView {
+            return view.tagDescription + ".safeAreaLayoutGuide"
+        } else {
+            return nil
+        }
+    }
+    
+    var constantShort: String? {
+        if constant < 0 {
+            return constant.description
+        } else if constant > 0 {
+            return "+ \(constant.description)"
+        } else {
+            return nil
+        }
+    }
+    
+    var multiplierShort: String? {
+        if multiplier != 1.0 {
+            return "x \(multiplier.description)"
+        } else {
+            return nil
+        }
+    }
+    
+    public var shortDescription: String {
+        var shorts: [String] = []
+        if let firstShort = firstShort {
+            shorts.append(firstShort + ".\(firstAttribute.description)")
+        }
+        shorts.append(relation.shortDescription)
+        if let secondShort = secondShort {
+            shorts.append(secondShort + ".\(secondAttribute.description)")
+        }
+        if let constantShort = constantShort {
+            shorts.append(constantShort)
+        }
+        if let multiplierShort = multiplierShort {
+            shorts.append(multiplierShort)
+        }
+        return shorts.joined(separator: " ")
+    }
+}
+
 extension NSLayoutConstraint.Attribute: CustomStringConvertible, CustomDebugStringConvertible {
     public var description: String {
         switch self {
@@ -138,6 +197,18 @@ extension NSLayoutConstraint.Relation: CustomStringConvertible, CustomDebugStrin
             return "NSLayoutConstraint.Relation.greaterThanOrEqual"
         @unknown default:
             return "NSLayoutConstraint.Relation.unknown"
+        }
+    }
+    var shortDescription: String {
+        switch self {
+        case .equal:
+            return "=="
+        case .greaterThanOrEqual:
+            return ">="
+        case .lessThanOrEqual:
+            return "<="
+        @unknown default:
+            return "?"
         }
     }
 }
