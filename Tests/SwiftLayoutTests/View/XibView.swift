14ba1a7c8389d0c7c63528ca6adb3bf82f2b2cab
oozoofrog <oozoofrog@gmail.com>
2022-03-18 19:42:24 +0900

    update xib

--- Tests/SwiftLayoutTests/View/XibView.swift
+++ Tests/SwiftLayoutTests/View/XibView.swift
@@ -7,6 +7,22 @@
 
 import UIKit
 
+@objcMembers
 class XibView: UIView {
    
+    @IBOutlet var navigationView: UIView!
+    @IBOutlet var payView: UIView!
+    @IBOutlet var gridTop: UIView!
+    @IBOutlet var gridBottom: UIView!
+    @IBOutlet var newsView: UIView!
+    @IBOutlet var bannerView: UIView!
+    @IBOutlet var optionsView: UIView!
+    
+    @IBOutlet var navigationTitleLabel: UILabel!
+    
+    
+    @IBOutlet var musicButton: UIButton!
+    @IBOutlet var settingButton: UIButton!
+    
+    
 }
