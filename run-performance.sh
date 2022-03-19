33f57f32f560ff936d238ac4cbf7a0b220f87368
oozoofrog <oozoofrog@gmail.com>
2022-03-19 12:57:22 +0900

    using build in run-performance.sh

--- run-performance.sh
+++ run-performance.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xcrun simctl create swiftlayouttest "iPhone 13 Pro Max"
-xcodebuild clean test -quiet -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest' OTHER_SWIFT_FLAGS="-Xfrontend -debug-time-function-bodies -Xfrontend -warn-long-expression-type-checking=10 -Xfrontend -warn-long-function-bodies=10" |
+xcodebuild clean build -quiet -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest' OTHER_SWIFT_FLAGS="-Xfrontend -debug-time-function-bodies -Xfrontend -warn-long-expression-type-checking=10 -Xfrontend -warn-long-function-bodies=10" |
         grep -o "^\d*.\d*ms\t[^$]*$" |
         awk '!visited[$0]++' |
         sed -e "s|$(pwd)/||" |
