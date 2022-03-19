d204441e39144b019f77dfe52066d6c2380007d4
gmlwhdtjd <gmlwhdtjd@naver.com>
2022-03-19 11:02:57 +0900

    Separated LayoutExplorer into separate file

--- Sources/SwiftLayout/Core/Layout/LayoutElements.swift
+++ Sources/SwiftLayout/Core/Layout/LayoutElements.swift
@@ -29,33 +29,3 @@ class LayoutElements {
     }
 }
 
-private enum LayoutExplorer {
-    struct Component {
-        var superView: UIView? = nil
-        var view: UIView
-        var anchors: Anchors? = nil
-    }
-    
-    typealias TraversalHandler = (_ layout: Layout, _ superview: UIView?) -> Void
-    
-    static func components(layout: Layout) -> [Component] {
-        var elements: [Component] = []
-        
-        traversal(layout: layout, superview: nil) { layout, superview in
-            if let view = layout.view {
-                elements.append(Component(superView: superview, view: view, anchors: layout.anchors))
-            }
-        }
-        
-        return elements
-    }
-    
-    static func traversal(layout: Layout, superview: UIView?, handler: TraversalHandler) {
-        handler(layout, superview)
-        
-        let nextSuperview = layout.view ?? superview
-        layout.sublayouts.forEach { layout in
-            traversal(layout: layout, superview: nextSuperview, handler: handler)
-        }
-    }
-}
---
+++ Sources/SwiftLayout/Core/Layout/LayoutExplorer.swift
@@ -0,0 +1,39 @@
+//
+//  LayoutExplorer.swift
+//  
+//
+//  Created by aiden_h on 2022/03/19.
+//
+
+import UIKit
+
+enum LayoutExplorer {
+    struct Component {
+        var superView: UIView? = nil
+        var view: UIView
+        var anchors: Anchors? = nil
+    }
+    
+    typealias TraversalHandler = (_ layout: Layout, _ superview: UIView?) -> Void
+    
+    static func components(layout: Layout) -> [Component] {
+        var elements: [Component] = []
+        
+        traversal(layout: layout, superview: nil) { layout, superview in
+            if let view = layout.view {
+                elements.append(Component(superView: superview, view: view, anchors: layout.anchors))
+            }
+        }
+        
+        return elements
+    }
+    
+    static func traversal(layout: Layout, superview: UIView?, handler: TraversalHandler) {
+        handler(layout, superview)
+        
+        let nextSuperview = layout.view ?? superview
+        layout.sublayouts.forEach { layout in
+            traversal(layout: layout, superview: nextSuperview, handler: handler)
+        }
+    }
+}
