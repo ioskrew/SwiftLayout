c638cf70d4d8149046ba26bd47acd1ee6c1a7771
oozoofrog <oozoofrog@gmail.com>
2022-03-19 12:54:25 +0900

    update run-performance for sorting

--- run-performance.sh
+++ run-performance.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 xcrun simctl create swiftlayouttest "iPhone 13 Pro Max"
-xcodebuild clean test -quiet -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest' OTHER_SWIFT_FLAGS="-Xfrontend -debug-time-function-bodies -Xfrontend -warn-long-expression-type-checking=10 -Xfrontend -warn-long-function-bodies=10" | grep -e '^[0-9]'
+xcodebuild clean test -quiet -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest' OTHER_SWIFT_FLAGS="-Xfrontend -debug-time-function-bodies -Xfrontend -warn-long-expression-type-checking=10 -Xfrontend -warn-long-function-bodies=10" |
+        grep -o "^\d*.\d*ms\t[^$]*$" |
+        awk '!visited[$0]++' |
+        sed -e "s|$(pwd)/||" |
+        grep -v "Pods" |
+        sort -n
 xcrun simctl delete swiftlayouttest
