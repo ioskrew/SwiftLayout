c2b432b75855dedd2afb261eb4cc57c1a5963a5c
oozoofrog <oozoofrog@gmail.com>
2022-03-20 00:47:56 +0900

    run-performance testing only performancetests

--- run-performance.sh
+++ run-performance.sh
@@ -2,7 +2,7 @@
 set -e
 xcrun simctl create swiftlayouttest "iPhone 13 Pro Max" >/dev/null
 
-xcodebuild test -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest'
+xcodebuild test -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest' -only-testing:SwiftLayoutTests/PerformanceTests
 
 xcodebuild clean build -quiet -scheme SwiftLayout -sdk iphonesimulator -destination 'name=swiftlayouttest' OTHER_SWIFT_FLAGS="-Xfrontend -warn-long-expression-type-checking=100" 2>/dev/null
 
