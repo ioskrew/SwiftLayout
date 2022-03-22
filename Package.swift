ed24b1bc311b6abd57343c9d9f80f57425ee358d
oozoofrog <oozoofrog@gmail.com>
2022-03-23 00:01:58 +0900

    remove xcframework things

--- Package.swift
+++ Package.swift
@@ -8,21 +8,16 @@ let package = Package(
     products: [
         .library(
             name: "SwiftLayout",
-            targets: ["SwiftLayout"]),
-        .library(
-            name: "SwiftLayoutFramework",
-            targets: ["SwiftLayoutFramework"])
+            targets: ["SwiftLayout"])
     ],
     dependencies: [],
     targets: [
         .target(
             name: "SwiftLayout",
             dependencies: []),
-        .binaryTarget(name: "SwiftLayoutFramework",
-                      path: "Binary/SwiftLayoutFramework.xcframework"),
         .testTarget(
             name: "SwiftLayoutTests",
             dependencies: ["SwiftLayout"],
-            resources: [.process("Resources/XibView.xib")]),
+            resources: [.process("Resources/XibView.xib")])
     ]
 )
