a6242d206b1e8b486af69086ff8d10d84ae347b1
oozoofrog <oozoofrog@gmail.com>
2022-04-04 20:24:24 +0900

    update SLTAssertView

--- Tests/SwiftLayoutTests/Util/XCTViewAssert.swift
+++ Tests/SwiftLayoutTests/Util/XCTViewAssert.swift
@@ -9,23 +9,30 @@ import Foundation
 import SwiftLayout
 import XCTest
 
-public func SLTAssertView(_ view: UIView, superview: UIView?, subviews: [UIView] = [], file: StaticString = #filePath, line: UInt = #line) {
-    if view.superview != superview {
-        if let superviewForView = view.superview, let superview = superview {
-            XCTFail("view<\(view.testDescription)>.superview(\(superviewForView.testDescription)) is not equal with superview<\(superview.testDescription)>", file: file, line: line)
-            return
-        } else if let superviewForView = view.superview {
-            XCTFail("view<\(view.testDescription)>.superview<\(superviewForView.testDescription)> is not equal with superview<nil>", file: file, line: line)
-            return
-        } else if let superview = superview {
-            XCTFail("view<\(view.testDescription)>.superview<nil> is not equal with superview<\(superview.testDescription)>", file: file, line: line)
-            return
+public func SLTAssertView(_ view: @autoclosure () throws -> UIView, superview: @autoclosure () throws -> UIView?, subviews: @autoclosure () throws -> [UIView]?, file: StaticString = #filePath, line: UInt = #line) {
+    do {
+        let view = try view()
+        let superview = try superview()
+        let subviews = try subviews() ?? []
+        if view.superview != superview {
+            if let superviewForView = view.superview, let superview = superview {
+                XCTFail("view<\(view.testDescription)>.superview(\(superviewForView.testDescription)) is not equal with superview<\(superview.testDescription)>", file: file, line: line)
+                return
+            } else if let superviewForView = view.superview {
+                XCTFail("view<\(view.testDescription)>.superview<\(superviewForView.testDescription)> is not equal with superview<nil>", file: file, line: line)
+                return
+            } else if let superview = superview {
+                XCTFail("view<\(view.testDescription)>.superview<nil> is not equal with superview<\(superview.testDescription)>", file: file, line: line)
+                return
+            }
         }
-    }
-    let subviewsInView = view.subviews.map(\.testDescription)
-    let subviewsForTest = subviews.map(\.testDescription)
-    if !subviewsInView.elementsEqual(subviewsForTest) {
-        XCTFail("view<\(view.testDescription)>.subviews(\(subviewsInView.count))[\(subviewsInView.joined(separator: ", "))] is not equal with subviews(\(subviewsForTest.count))[\(subviewsForTest.joined(separator: ", "))]", file: file, line: line)
+        let subviewsInView = view.subviews.map(\.testDescription)
+        let subviewsForTest = subviews.map(\.testDescription)
+        if !subviewsInView.elementsEqual(subviewsForTest) {
+            XCTFail("view<\(view.testDescription)>.subviews(\(subviewsInView.count))[\(subviewsInView.joined(separator: ", "))] is not equal with subviews(\(subviewsForTest.count))[\(subviewsForTest.joined(separator: ", "))]", file: file, line: line)
+        }
+    } catch {
+        XCTFail(error.localizedDescription, file: file, line: line)
     }
 }
 
