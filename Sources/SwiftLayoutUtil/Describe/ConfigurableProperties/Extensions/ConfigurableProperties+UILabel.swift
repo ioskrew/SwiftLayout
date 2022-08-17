800dc2133eec298dfe9930610e4de945a48a528f
oozoofrog <oozoofrog@gmail.com>
2022-08-17 14:19:58 +0900

    add version check and fix misspellings

--- Sources/SwiftLayoutUtil/Describe/ConfigurableProperties/Extensions/ConfigurableProperties+UILabel.swift
+++ Sources/SwiftLayoutUtil/Describe/ConfigurableProperties/Extensions/ConfigurableProperties+UILabel.swift
@@ -12,7 +12,7 @@ extension ConfigurableProperties {
     }
     
     private func uiLabelDefaultConfigurableProperties(defaultReferenceView label: UILabel) -> [ConfigurableProperty] {
-        return [
+        var configurableProperties: [ConfigurableProperty] = [
             .property(keypath: \.adjustsFontForContentSizeCategory, defaultReferenceView: label) { "$0.adjustsFontForContentSizeCategory = \($0)" },
             .property(keypath: \.adjustsFontSizeToFitWidth, defaultReferenceView: label) { "$0.adjustsFontSizeToFitWidth = \($0)" },
             .property(keypath: \.allowsDefaultTighteningForTruncation, defaultReferenceView: label) { "$0.allowsDefaultTighteningForTruncation = \($0)" },
@@ -21,15 +21,21 @@ extension ConfigurableProperties {
             .property(keypath: \.highlightedTextColor, defaultReferenceView: label) { "$0.highlightedTextColor = \($0.configuration)" },
             .property(keypath: \.isEnabled, defaultReferenceView: label) { "$0.isEnabled = \($0)" },
             .property(keypath: \.isHighlighted, defaultReferenceView: label) { "$0.isHighlighted = \($0)" },
-            .property(keypath: \.lineBreakMode, defaultReferenceView: label) { "$0.lineBreakMode = .\($0.configuration)" },
+            .property(keypath: \.lineBreakMode, defaultReferenceView: label) { "$0.lineBreakMode = \($0.configuration)" },
             .property(keypath: \.minimumScaleFactor, defaultReferenceView: label) { "$0.minimumScaleFactor = \($0)" },
             .property(keypath: \.numberOfLines, defaultReferenceView: label) { "$0.numberOfLines = \($0)" },
             .property(keypath: \.shadowColor, defaultReferenceView: label) { "$0.shadowColor = \($0.configuration)" },
             .property(keypath: \.shadowOffset, defaultReferenceView: label) { "$0.shadowOffset = CGSize(width: \($0.width), height: \($0.height))" },
-            .property(keypath: \.showsExpansionTextWhenTruncated, defaultReferenceView: label) { "$0.showsExpansionTextWhenTruncated = \($0)" },
             .property(keypath: \.text, defaultReferenceView: label) { "$0.text = \($0.configuration)"},
             .property(keypath: \.textAlignment, defaultReferenceView: label) { "$0.textAlignment = \($0.configuration)" },
             .property(keypath: \.textColor, defaultReferenceView: label) { "$0.textColor = \($0.configuration)" },
         ]
+        
+        if #available(iOS 14.0, *), UIDevice.current.userInterfaceIdiom == .mac {
+            configurableProperties.append(contentsOf: [
+                .property(keypath: \.showsExpansionTextWhenTruncated, defaultReferenceView: label) { "$0.showsExpansionTextWhenTruncated = \($0)" }
+            ])
+        }
+        return configurableProperties
     }
 }
--- Tests/SwiftLayoutUtilTests/ViewPrinterTests.swift
+++ Tests/SwiftLayoutUtilTests/ViewPrinterTests.swift
@@ -969,7 +969,9 @@ extension ViewPrinterTests {
                 $0.numberOfLines = 3
                 $0.isEnabled = false
                 $0.isHighlighted = true
-                $0.showsExpansionTextWhenTruncated = true
+                if #available(iOS 14.0, *), UIDevice.current.userInterfaceIdiom == .mac {
+                    $0.showsExpansionTextWhenTruncated = true
+                }
                 $0.baselineAdjustment = .alignCenters
                 $0.lineBreakMode = .byTruncatingHead
                 $0.adjustsFontSizeToFitWidth = true
@@ -981,32 +983,63 @@ extension ViewPrinterTests {
             }
         }.active()
 
-        let expect = """
-        root.config {
-            $0.accessibilityIdentifier = "root"
-        }.sublayout {
-            child.config {
-                $0.accessibilityIdentifier = "child"
-                $0.adjustsFontForContentSizeCategory = true
-                $0.adjustsFontSizeToFitWidth = true
-                $0.allowsDefaultTighteningForTruncation = true
-                $0.baselineAdjustment = .alignCenters
-                $0.font = /* Modified! Check it manually. (fontName: .SFUI-Semibold, pointSize: 16.0) */
-                $0.highlightedTextColor = /* Modified! Check it manually. (hex: #0000FF, alpha: 1.0) */
-                $0.isEnabled = false
-                $0.isHighlighted = true
-                $0.lineBreakMode = ..byTruncatingHead
-                $0.minimumScaleFactor = 0.5
-                $0.numberOfLines = 3
-                $0.shadowColor = /* Modified! Check it manually. (hex: #996633, alpha: 1.0) */
-                $0.shadowOffset = CGSize(width: 0.0, height: 0.0)
-                $0.showsExpansionTextWhenTruncated = true
-                $0.text = "text_child"
-                $0.textAlignment = .left
-                $0.textColor = /* Modified! Check it manually. (hex: #7F7F7F, alpha: 1.0) */
+        let expect: String
+        if #available(iOS 14.0, *), UIDevice.current.userInterfaceIdiom == .mac {
+            expect =
+            """
+            root.config {
+                $0.accessibilityIdentifier = "root"
+            }.sublayout {
+                child.config {
+                    $0.accessibilityIdentifier = "child"
+                    $0.adjustsFontForContentSizeCategory = true
+                    $0.adjustsFontSizeToFitWidth = true
+                    $0.allowsDefaultTighteningForTruncation = true
+                    $0.baselineAdjustment = .alignCenters
+                    $0.font = /* Modified! Check it manually. (fontName: .SFUI-Semibold, pointSize: 16.0) */
+                    $0.highlightedTextColor = /* Modified! Check it manually. (hex: #0000FF, alpha: 1.0) */
+                    $0.isEnabled = false
+                    $0.isHighlighted = true
+                    $0.lineBreakMode = .byTruncatingHead
+                    $0.minimumScaleFactor = 0.5
+                    $0.numberOfLines = 3
+                    $0.shadowColor = /* Modified! Check it manually. (hex: #996633, alpha: 1.0) */
+                    $0.shadowOffset = CGSize(width: 0.0, height: 0.0)
+                    $0.showsExpansionTextWhenTruncated = true
+                    $0.text = "text_child"
+                    $0.textAlignment = .left
+                    $0.textColor = /* Modified! Check it manually. (hex: #7F7F7F, alpha: 1.0) */
+                }
             }
+            """
+        } else {
+            expect =
+            """
+            root.config {
+                $0.accessibilityIdentifier = "root"
+            }.sublayout {
+                child.config {
+                    $0.accessibilityIdentifier = "child"
+                    $0.adjustsFontForContentSizeCategory = true
+                    $0.adjustsFontSizeToFitWidth = true
+                    $0.allowsDefaultTighteningForTruncation = true
+                    $0.baselineAdjustment = .alignCenters
+                    $0.font = /* Modified! Check it manually. (fontName: .SFUI-Semibold, pointSize: 16.0) */
+                    $0.highlightedTextColor = /* Modified! Check it manually. (hex: #0000FF, alpha: 1.0) */
+                    $0.isEnabled = false
+                    $0.isHighlighted = true
+                    $0.lineBreakMode = .byTruncatingHead
+                    $0.minimumScaleFactor = 0.5
+                    $0.numberOfLines = 3
+                    $0.shadowColor = /* Modified! Check it manually. (hex: #996633, alpha: 1.0) */
+                    $0.shadowOffset = CGSize(width: 0.0, height: 0.0)
+                    $0.text = "text_child"
+                    $0.textAlignment = .left
+                    $0.textColor = /* Modified! Check it manually. (hex: #7F7F7F, alpha: 1.0) */
+                }
+            }
+            """
         }
-        """
 
         let result = ViewPrinter(root, options: .withViewConfig).description
         XCTAssertEqual(result, expect)
