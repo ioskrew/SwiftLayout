f0d5bb9792fd4cd6335b7461185bd28b456ed999
oozoofrog <oozoofrog@gmail.com>
2022-08-17 11:13:34 +0900

    type erase from TupleLayout

--- Sources/SwiftLayout/Core/Layout/Layouts/TupleLayout.swift
+++ Sources/SwiftLayout/Core/Layout/Layouts/TupleLayout.swift
@@ -1,14 +1,29 @@
-public struct TupleLayout<L>: Layout {
+public struct TupleLayout: Layout {
     
-    private let layout: L
+    public var sublayouts: [Layout]
     
-    init(_ layout: L) {
-        self.layout = layout
+    init<L: Layout, L1: Layout>(_ l: L, _ l1: L1) {
+        self.sublayouts = [l, l1]
     }
     
-    public var sublayouts: [Layout] {
-        Mirror(reflecting: layout).children.compactMap { (_, value) in
-            value as? Layout
-        }
+    init<L: Layout, L1: Layout, L2: Layout>(_ l: L, _ l1: L1, _ l2: L2) {
+        self.sublayouts = [l, l1, l2]
     }
+    
+    init<L: Layout, L1: Layout, L2: Layout, L3: Layout>(_ l: L, _ l1: L1, _ l2: L2, _ l3: L3) {
+        self.sublayouts = [l, l1, l2, l3]
+    }
+    
+    init<L: Layout, L1: Layout, L2: Layout, L3: Layout, L4: Layout>(_ l: L, _ l1: L1, _ l2: L2, _ l3: L3, _ l4: L4) {
+        self.sublayouts = [l, l1, l2, l3, l4]
+    }
+    
+    init<L: Layout, L1: Layout, L2: Layout, L3: Layout, L4: Layout, L5: Layout>(_ l: L, _ l1: L1, _ l2: L2, _ l3: L3, _ l4: L4, _ l5: L5) {
+        self.sublayouts = [l, l1, l2, l3, l4, l5]
+    }
+    
+    init<L: Layout, L1: Layout, L2: Layout, L3: Layout, L4: Layout, L5: Layout, L6: Layout>(_ l: L, _ l1: L1, _ l2: L2, _ l3: L3, _ l4: L4, _ l5: L5, _ l6: L6) {
+        self.sublayouts = [l, l1, l2, l3, l4, l5, l6]
+    }
+    
 }
