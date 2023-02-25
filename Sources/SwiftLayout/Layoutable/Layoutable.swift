b77f131c0db050fdcaf2487cbaa059b0a54783fb
Lee Hui-Jong <gmlwhdtjd@naver.com>
2023-02-26 00:48:06 +0900

    rename LayoutableMethodWrapper to LayoutMethodWrapper and code cleanup

--- Sources/SwiftLayout/Layoutable/Layoutable.swift
+++ Sources/SwiftLayout/Layoutable/Layoutable.swift
@@ -5,7 +5,7 @@
 //  Created by oozoofrog on 2022/02/06.
 //
 
-public protocol Layoutable: AnyObject, LayoutElement {
+public protocol Layoutable: AnyObject, LayoutBase {
     associatedtype LayoutBody: Layout
     var activation: Activation? { get set }
     @LayoutBuilder var layout: LayoutBody { get }
